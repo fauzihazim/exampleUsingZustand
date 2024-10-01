45ddc6b1e9d0b97d3cbb325c0f6d5d2986fdf019
fauzihazim <fauzihazim28@gmail.com>
2024-10-01 16:48:32 +0700

    fix: adding data to array of object

--- src/pages/index/Index.js
+++ src/pages/index/Index.js
@@ -1,28 +1,26 @@
 import React, { useState } from 'react';
-import useStore from './store/UsePerson';// Adjust the import path as necessary
+import useStore from './store/UsePerson';
 
 const ItemList = () => {
   const items = useStore((state) => state.items);
   const addItem = useStore((state) => state.addItem);
-  const [newItem, setNewItem] = useState('');
+  const [ firstName, setFirstName ] = useState('');
+  const [ lastName, setLastName ] = useState('');
 
   const handleAddItem = () => {
-    addItem({ id: items.length + 1, name: newItem });
-    setNewItem('');
+    addItem({ id: items.length + 1, firstName: firstName, lastName: lastName });
+    setFirstName('');
+    setLastName('');
   };
 
   return (
     <div>
-      <input
-        type="text"
-        value={newItem}
-        onChange={(e) => setNewItem(e.target.value)}
-        placeholder="Add new item"
-      />
+      <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} placeholder="Add First Name" />
+      <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} placeholder='Add Last Name' />
       <button onClick={handleAddItem}>Add Item</button>
       <ul>
         {items.map((item) => (
-          <li key={item.id}>{item.name}</li>
+          <li key={item.id}>id: {item.id}, first name: {item.firstName}, last name: {item.lastName}</li>
         ))}
       </ul>
     </div>
